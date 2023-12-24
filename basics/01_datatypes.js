68d1d78b162f210ec560c3e8492745be29c0e63e
Yashanshav Raj <yashanshavvrajpurohit@gmail.com>
2023-12-24 17:43:50 +0530

    stack and heap

---
+++ basics/01_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using node js, not browser
+
+console.log(3 + 3); 
+
+console.log("yash")
+
+let name = "yash";
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+
+// numer => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
