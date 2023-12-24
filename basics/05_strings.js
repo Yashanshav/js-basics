048355afd0ed25b3a46eddd443bfcd1069a61686
Yashanshav Raj <yashanshavvrajpurohit@gmail.com>
2023-12-24 18:09:50 +0530

    strings

---
+++ basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = 'yash';
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value"); //Outdated
+
+console.log(`My name is ${name} and my count is ${repoCount}`);
+
+const gameName = new String('hiteshhc')
+
+// console.log(gameName[0]); //Not an array but object only with key value pairs
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.indexOf('hhc'));
