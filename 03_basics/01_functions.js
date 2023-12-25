07aa4bb788dd6c08e5d7b349b459c84143a3d874
Yashanshav Raj <yashanshavvrajpurohit@gmail.com>
2023-12-25 22:10:30 +0530

    functions part 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,8 +38,58 @@ function newLoginUserMessage(username) {
     return `${username} is logged in` 
 }
 
-console.log(newLoginUserMessage())
+// console.log(newLoginUserMessage())
 
 function loginUserMessage(username = 'sam') { // giving default value 
     return `${username} is logged in`
 }
+
+function calculateCartPrice(num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(100, 200)) //Output: 100
+
+function ccp2(...num1) { // Now this is rest operator
+    return num1
+}
+
+// console.log(ccp2(100, 200, 300, 400)) //Output: [ 100, 200, 300, 400 ]
+
+function ccp3(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(ccp3(100, 200, 300, 400)) //Output: [ 300, 400 ] (as we return num1 only)
+
+const user = {
+    name: "yash",
+    price: 199
+}
+
+const user2 = {
+    name: "yash",
+    prices: 199
+}
+
+function handleObject(anyObject) {
+    console.log(`My name is ${anyObject.name} and the price is ${anyObject.price}`)
+}
+
+// handleObject() //throws error as object is needed
+// handleObject(user) //Output: My name is yash and the price is 199
+// handleObject(user2) //Output: My name is yash and the price is undefined
+
+handleObject({
+    name: "sam",
+    price: 399
+}) 
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray)); //Output: 400
+console.log(returnSecondValue([200, 400, 500, 1000])); // Output: 400
