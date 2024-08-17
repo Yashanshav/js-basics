09a8c43b2f0a8c4b05d1468db66e0a5348694e66
Yashanshav <yashanshavvrajpurohit@gmail.com>
2024-08-18 01:37:11 +0530

    add space to map

--- 04_iterations/02_map.js
+++ 04_iterations/02_map.js
@@ -5,7 +5,7 @@ const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const newNums2 = myNumbers
     .map((num) => num * 10)
-    .map( (num) => num + 1)
+    .map( (num) => num + 1) 
     .filter( (num) => num >= 40)
 
 console.log(newNums2) // [41, 51,  61, 71, 81, 91, 101]
